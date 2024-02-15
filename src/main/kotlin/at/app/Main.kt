a1841c774f02e3cb31b9d283ecf0e5b16f46e59b
Undertaker <achim.unterkofler@alphagate.eu>
2024-02-15 22:44:29 +0100

    for jan

--- src/main/kotlin/at/app/Main.kt
+++ src/main/kotlin/at/app/Main.kt
@@ -1,4 +1,4 @@
-package org.example
+package org.example.at
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,7 +6,12 @@ fun main() {
     val name = "Achim"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + " enter 2 numbers devided by a whitespace: ")
+    println("""
+        |    |  |¯¯¯¯  |      |  /---\    /¯¯¯¯
+        |----|  |----  |      | |     |  /____
+        |    |  |____  |____  |  \---/   ____/
+        ©
+        """)
 
     val (a, b) = readln().split(' ').map(String::toInt)
     println(a + b)
