27f70f371f6dbc2dfc174c7ebf662cf62b87897e
Undertaker <achim.unterkofler@alphagate.eu>
2024-02-15 23:04:34 +0100

    addon

--- src/main/kotlin/at/app/Main.kt
+++ src/main/kotlin/at/app/Main.kt
@@ -6,13 +6,24 @@ fun main() {
     val name = "Achim"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
+    System.out.println("\u001B[32m");
     println("""
         |    |  |¯¯¯¯  |      |  /---\    /¯¯¯¯
         |----|  |----  |      | |     |  /____
         |    |  |____  |____  |  \---/   ____/  ©     
       /¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯/
-     /       WELCOME FRIEND                                /  
-    /_____________________________________________________/
+     /       WELCOME FRIEND -------------------------------->  FOR MENUE PRESS ...................... M
+    /_____________________________________________________/    FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M
+                                                               FOR MENUE PRESS ...................... M                                                              
         """)
 
     val (a, b) = readln().split(' ').map(String::toInt)
