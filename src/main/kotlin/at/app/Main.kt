37a26c2baef0eb9b1e7c0b818571de4ae03212f6
jbr <jan.m.brandner@gmail.com>
2024-02-20 21:58:48 +0100

    commit

--- src/main/kotlin/at/app/Main.kt
+++ src/main/kotlin/at/app/Main.kt
@@ -3,41 +3,20 @@ package org.example.at
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.required
-import kotlinx.cli.*
 
-class CommandLineOptions {
-    var input by parser.option(ArgType.String, shortName = "i", description = "Der Eingabedateipfad").required()
-    var output by parser.option(ArgType.String, shortName = "o", description = "Der Ausgabedateipfad").required()
+class CommandLineOptions(parser: ArgParser) {
+    val input by parser.option(ArgType.String, shortName = "i", description = "Der Eingabedateipfad").required()
+    val output by parser.option(ArgType.String, shortName = "o", description = "Der Ausgabedateipfad").required()
 }
 
 fun main(args: Array<String>) {
-    val options = CommandLineOptions()
     val parser = ArgParser("YourApplication")
+    val options = CommandLineOptions(parser)
 
     try {
         parser.parse(args)
 
         // Hier können Sie die initiale Konfiguration durchführen oder Aufgaben vor der Schleife erledigen.
-
-        System.out.println("\u001B[32m");
-        println("""
-        |    |  |¯¯¯¯  |      |  /¯¯¯\    /¯¯¯¯
-        |----|  |----  |      | |     |  /____
-        |    |  |____  |____  |  \___/   ____/  ©     
-      /¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯/
-     /       WELCOME FRIEND -------------------------------->  FOR MENUE PRESS ...................... M
-    /_____________________________________________________/    FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M
-                                                               FOR MENUE PRESS ...................... M                                                              
-        """)
 
         // Schleife für die dauerhafte Ausführung
         while (true) {
@@ -53,6 +32,7 @@ fun main(args: Array<String>) {
     } catch (e: Exception) {
         // Wenn ein Fehler bei der Verarbeitung der Argumente auftritt, zeigen Sie die Fehlermeldung und die Verwendung an.
         System.err.println(e.message)
-        parser.printHelp()
+        
     }
 }
+
